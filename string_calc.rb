73a124365bae35c8df714709bdd9565a6bbd42af
Sabahat25 <sabahat@Sabahat.localdomain>
2025-08-29 16:59:09 +0530

    final

--- string_calc.rb
+++ string_calc.rb
@@ -1,17 +1,17 @@
 module StringCalc
   class << self
 
-	def add(*numbers)
-		str = numbers.flatten.join(',')
+	def add(*args)
+		str = args.flatten.join(',')
 		return 0 if str.empty?
 
 		if str.start_with?("//")
-			delim, str = str[2..].split("\n", 2)
+			separator, str = str[2..].split("\n", 2)
 		else
-			delim = /,|\n/
+			separator = /,|\n/
 		end
 
-		nums = str.split(delim).map(&:to_i)
+		nums = str.split(separator).map(&:to_i)
 		negatives = nums.select(&:negative?)
 		raise "negative numbers not allowed: #{negatives.join(',')}" if negatives.any?
 		nums.sum

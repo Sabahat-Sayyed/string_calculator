14f60e44ef23b9763d3cc7ab4c6cc7cf939e01a0
Sabahat25 <sabahat@Sabahat.localdomain>
2025-08-29 15:59:46 +0530

    Support different delimiters

--- string_calc.rb
+++ string_calc.rb
@@ -2,7 +2,15 @@ module StringCalc
   class << self
 
 	def add(*numbers)
-		numbers.flatten.flat_map { |n| n.to_s.split(/,|\n/) }.map(&:to_i).sum
+		str = numbers.flatten.join(',')
+		return 0 if str.empty?
+
+		if str.start_with?("//")
+			delim, str = str[2..].split("\n", 2)
+		else
+			delim = /,|\n/
+		end
+		str.split(delim).sum { |n| n.to_i }
 	end
 
   end

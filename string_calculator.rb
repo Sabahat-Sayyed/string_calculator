0adb6fb7e2af8a1f7187ac9934531e1ba2198fae
Sabahat25 <sabahat@Sabahat.localdomain>
2025-10-05 23:52:17 +0530

    Support different seprators

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,15 @@
 module StringCalculator
   class << self
 	def add(*args)
-		args.flatten.flat_map { |n| n.to_s.split(/,|\n/) }.map(&:to_i).sum
+		str = args.flatten.join(',')
+		return 0 if str.empty?
+
+		if str.start_with?("//")
+			seprator, str = str[2..].split("\n", 2)
+			else
+			seprator = /,|\n/
+		end
+		str.split(seprator).sum { |n| n.to_i }
 	end
   end
 end

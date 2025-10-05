c4fbaed7e65dada79e964ddc731aeacc78b5ded4
Sabahat25 <sabahat@Sabahat.localdomain>
2025-10-05 23:53:04 +0530

    Negative numbers Exception

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,15 +1,21 @@
 module StringCalculator
   class << self
+
 	def add(*args)
 		str = args.flatten.join(',')
 		return 0 if str.empty?
 
 		if str.start_with?("//")
 			seprator, str = str[2..].split("\n", 2)
-			else
+		else
 			seprator = /,|\n/
 		end
-		str.split(seprator).sum { |n| n.to_i }
+
+		nums = str.split(seprator).map(&:to_i)
+		negatives = nums.select(&:negative?)
+		raise "negative numbers not allowed: #{negatives.join(',')}" if negatives.any?
+		nums.sum
 	end
+
   end
 end
